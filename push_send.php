5af12a0b752cf437dbb90b0ff17b02f4443f9d23
SetMyWed Reviews & Pricing <aruncobra007@gmail.com>
2026-09-26 18:06:39 +0530

    Fix push sender redirect loop

--- push_send.php
+++ push_send.php
@@ -12,7 +12,7 @@ if($title&&$body){
   $cfg=require __DIR__.'/config.php';
   $pdo=new PDO("mysql:host={$cfg['db']['host']};port={$cfg['db']['port']};dbname={$cfg['db']['name']};charset=utf8mb4",$cfg['db']['user'],$cfg['db']['pass'],[PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION,PDO::ATTR_DEFAULT_FETCH_MODE=>PDO::FETCH_ASSOC]);
   [$sent,$removed]=pushSend($pdo,$title,$body,$url);
-  header('Location:?view=admin&push_sent='.$sent);exit;
- }catch(Throwable $e){header('Location:?view=admin&push_error=1');exit;}
+  header('Location:/?view=admin&push_sent='.$sent);exit;
+ }catch(Throwable $e){header('Location:/?view=admin&push_error=1');exit;}
 }
-header('Location:?view=admin&push_error=1');exit;
+header('Location:/?view=admin&push_error=1');exit;
